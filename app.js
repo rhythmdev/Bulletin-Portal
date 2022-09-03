ded037c3f72af7f7fe114fdde76d3ff1416ca75e
rhythmdev <rhythmbiswas@gmail.com>
2022-09-03 19:54:50 +0600

    not found added

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const displayCatagories = (categories) => {
 
       li.innerHTML = `
                 
-                <a class="nav-link text-dark" href="#">${category.category_name}</a>
+                <a onclick="loadCategoriesData('${category.category_id}')" class="nav-link text-dark" href="#">${category.category_name}</a>
                
                `;
       allCategory.appendChild(li);
@@ -28,13 +28,63 @@ const displayCatagories = (categories) => {
 
 
 const loadCategoriesData = async(category_id) => {
+  try{
     const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
     const data = await res.json();
-    console.log(data.data)
+    displayCatagoriesData(data.data)
+  }
+  catch (error) {
+    console.log(error)
+  }
+    
+
 }
 
-const displayCatagoriesData = (elementData)=>{
+const displayCatagoriesData = elements =>{
+     //console.log(elements)
+// total news found
+
+const totalFound = document.getElementById('total-category');
+totalFound.textContent = '';
+ const totalDiv = document.createElement('div')
+ totalDiv.innerHTML = `
+<h5>${elements.length} items found for category ('${elements.category_name}')</h5>
+`
+totalFound.appendChild(totalDiv)
+
+const newsContainer = document.getElementById('news-container');
+newsContainer.textContent = '';
+// display no news
+const noNews = document.getElementById('not-found-message');
+if(elements.length === 0){
+  noNews.classList.remove('d-none')
+}
+else{
+  noNews.classList.add('d-none')
+}
+// display all news
+elements.forEach( element => {
+    const newsDiv = document.createElement('div');
+    newsDiv.classList.add('card')
+    newsDiv.innerHTML = `
+    <div class="row g-4">
+          <div class="col-md-4">
+            <img src="${element.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${element.title}</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div> 
     
+    `
+    newsContainer.appendChild(newsDiv)
+})
+
 }
 
 
