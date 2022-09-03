0a26998380f0bac4b091df2a49a18534c45ea502
rhythmdev <rhythmbiswas@gmail.com>
2022-09-03 21:36:54 +0600

    card redesign

--- app.js
+++ app.js
@@ -26,66 +26,82 @@ const displayCatagories = (categories) => {
   }
 };
 
-
-const loadCategoriesData = async(category_id) => {
-  try{
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
+const loadCategoriesData = async (category_id) => {
+  try {
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/news/category/${category_id}`
+    );
     const data = await res.json();
-    displayCatagoriesData(data.data)
+    displayCatagoriesData(data.data);
+  } catch (error) {
+    console.log(error);
   }
-  catch (error) {
-    console.log(error)
+};
+
+const displayCatagoriesData = (elements) => {
+  //console.log(elements)
+  // total news found
+
+  const totalFound = document.getElementById("total-category");
+  totalFound.textContent = "";
+  const totalDiv = document.createElement("div");
+  totalDiv.innerHTML = `
+<h5>${
+    elements.length ? elements.length : "No data"
+  } items found for category '${elements.category_name}'</h5>
+`;
+  totalFound.appendChild(totalDiv);
+
+  const newsContainer = document.getElementById("news-container");
+  newsContainer.textContent = "";
+  // display no news
+  const noNews = document.getElementById("not-found-message");
+  if (elements.length === 0) {
+    noNews.classList.remove("d-none");
+  } else {
+    noNews.classList.add("d-none");
   }
-    
-
-}
-
-const displayCatagoriesData = elements =>{
-     //console.log(elements)
-// total news found
-
-const totalFound = document.getElementById('total-category');
-totalFound.textContent = '';
- const totalDiv = document.createElement('div')
- totalDiv.innerHTML = `
-<h5>${elements.length} items found for category ('${elements.category_name}')</h5>
-`
-totalFound.appendChild(totalDiv)
-
-const newsContainer = document.getElementById('news-container');
-newsContainer.textContent = '';
-// display no news
-const noNews = document.getElementById('not-found-message');
-if(elements.length === 0){
-  noNews.classList.remove('d-none')
-}
-else{
-  noNews.classList.add('d-none')
-}
-// display all news
-elements.forEach( element => {
-    const newsDiv = document.createElement('div');
-    newsDiv.classList.add('card')
+  // display all news
+  elements.forEach((element) => {
+    const newsDiv = document.createElement("div");
+    newsDiv.classList.add("col");
     newsDiv.innerHTML = `
-    <div class="row g-4">
-          <div class="col-md-4">
-            <img src="${element.thumbnail_url}" class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">${element.title}</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <div class="card h-100 p-3">
+              <img src="${
+                element.thumbnail_url
+              }" class="card-img-top img-fluid h-50" alt="...">
+              <div class="card-body text-center">
+                <h5 class="card-title">${
+                  element.title ? element.title : "No data found"
+                }</h5>
+                <p class="card-text text-break mt-3">${
+                  element.details.slice(0, 180) + "....."
+                }</p>
+                
+              </div>
+              <div class="d-flex justify-content-around align-items-center text-center">
+                <div>
+                 <img src="${
+                   element.author.img ? element.author.img : "No data found"
+                 }" class="img-fluid rounded-circle" style="width: 35px;">
+                 <p>${
+                   element.author.name ? element.author.name : "No data found"
+                 }</p>
+                </div>
+                <div>
+                  <img src="/view.png" alt="image">
+                  <p>${
+                    element.total_view ? element.total_view : "No data found"
+                  }</p>
+                </div>
+              
+                <button class="show-btn">Show Details</button>
+               </div>
             </div>
-          </div>
-        </div>
-      </div> 
     
-    `
-    newsContainer.appendChild(newsDiv)
-})
-
-}
-
+    `;
+    newsContainer.appendChild(newsDiv);
+  });
+};
 
 loadCatagories();
